bcb27210f51c85ea217fa608ec33b6015ab8507f
nieyu <nieyuchina@gmail.com>
2019-02-16 20:43:59 +0800

    add iOS run environment

--- Tests/FisherYatesTests/XCTestManifests.swift
+++ Tests/FisherYatesTests/XCTestManifests.swift
@@ -1,9 +1,11 @@
 import XCTest
 
-#if !os(macOS)
+#if os(macOS) || os(iOS)
+#else
 public func allTests() -> [XCTestCaseEntry] {
     return [
         testCase(FisherYatesShuffleTest.allTests)
     ]
 }
 #endif
+
